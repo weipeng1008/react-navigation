e9ca028f5e2005c47f7b78b0a52f1aea1bab1e0e
weipeng1008 <57353633+weipeng1008@users.noreply.github.com>
2020-05-13 22:33:40 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Navigator from './routes/homeStack';
+
+export default function App() {
+  return (
+    <Navigator/>
+     
+  );
+}
+
